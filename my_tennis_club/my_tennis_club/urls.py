b7ead8a8955f979c22da95df2c939bf16e4a41b1
JStalynB <estalinxk3@gmail.com>
2024-04-04 18:56:19 -0500

    versión 2

--- my_tennis_club/my_tennis_club/urls.py
+++ my_tennis_club/my_tennis_club/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for brothers project.
+URL configuration for my_tennis_club project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,18 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("polls/", include("polls.urls")),
-    
+    path('', include('members.urls')),
+    path('admin/', admin.site.urls),
 ]
